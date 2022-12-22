d9700a6d264d334eda1d783c44ba681edf3e798b
rizwan <rizwan@rizwans-MBP.fios-router.home>
2022-12-22 17:11:13 -0500

    loadbalancer example

--- src/main/scala/FinagleDemo.scala
+++ src/main/scala/FinagleDemo.scala
@@ -1,10 +1,12 @@
-import com.twitter.finagle.{Http, ListeningServer, Service, SimpleFilter}
+import com.twitter.finagle.{Address, Http, ListeningServer, Name, Service, SimpleFilter}
 import com.twitter.finagle.http.{Method, Request, Response, Status}
 import com.twitter.util.{Await, Duration, Future, JavaTimer, Timer}
 
 object FinagleDemo {
   def main(args: Array[String]): Unit =
-    Await.ready(simpleHttpServer)
+    (9090 to 9092).map { port =>
+      simpleHttpServer(port)
+    }.foreach(Await.ready)
 
   // GET localhost:9090?name=rizwan
   def stringLengthService = new Service[Request, Response]:
@@ -17,8 +19,8 @@ object FinagleDemo {
         response
       }
 
-  def simpleHttpServer: ListeningServer =
-    Http.serve(":9090", stringLengthService)
+  def simpleHttpServer(port: Int): ListeningServer =
+    Http.serve(s":$port", debugFilter(s"service-$port").andThen(stringLengthService))
 }
 
 object SimpleClient {
@@ -31,6 +33,17 @@ object SimpleClient {
     response.onSuccess(resp => println(resp.getContentString()))
     response.onFailure(exp => exp.printStackTrace())
     Thread.sleep(2000)
+}
+
+object LoadBalancedClient {
+  def main(args: Array[String]): Unit =
+    val addresses = (9090 to 9092).map(port => Address("localhost", port))
+    val name: Name = Name.bound(addresses*)
+    val client = Http.newService(name, "client")
+    val requests = (1 to 20).map(i => Request(Method.Get, s"?name=${"rizwan" * i}"))
+    val responses = requests.map(req => client(req).map(_.getContentString))
+    Future.collect(responses).foreach(println)
+    Thread.sleep(5000)
 }
 
 // filters aka middleware aka interceptors
@@ -50,3 +63,12 @@ class TimeoutFilter[Req, Rep](timeout: Duration, timer: Timer) extends SimpleFil
   override def apply(request: Req, service: Service[Req, Rep]): Future[Rep] =
     service(request).within(timer, timeout)
 }
+
+def debugFilter(id: String): SimpleFilter[Request, Response] = new SimpleFilter[Request, Response] {
+  override def apply(request: Request, service: Service[Request, Response]): Future[Response] = {
+    println(s"[$id] received reqiest $request")
+    service(request)
+  }
+}
+
+// loadbalancers
